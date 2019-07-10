559a673943523fe8deab41c51bcc4300e4daf681
Boris Perepelita <boris.perepelita@intelligentbee.com>
2019-07-10 13:22:57 +0300

    Dmex build

---
+++ static/external.js
@@ -0,0 +1,52 @@
+/* eslint-disable */
+
+var clickTag = "";
+var bannerJson = {"hash":"bdh3qpmug","userId":"38120063","banner":{"properties":{"status":"publish","name":"Loading Ethermium","width":1600,"height":1200,"loop":false,"loopCount":0,"imageQuality":100,"bannerUrl":"","bannerSize":"Custom size","urlTarget":"_blank","useHandCursor":true,"useBannerEntireArea":true,"customSize":true,"presetSize":false,"backgroundColor":{"borderColor":"#3d3d3d","scolor":"#ffffff","type":"solid","gradColors":[{"c":"#ffffff","p":"0"},{"c":"#e1e6ec","p":"100"}],"rgradPos":null,"backgroundRotation":"270","contentScale":100,"scaleMode":"crop","useBorder":false,"horizontalAlign":"center","verticalAlign":"middle","contentOffsetX":50,"contentOffsetY":50,"url":null,"hqUrl":null},"lastId":142,"version":2,"useAsClickTag":false,"convertCustomFonts":false},"elements":[{"properties":{"backgroundColor":{"borderColor":"#3d3d3d","scolor":"rgba(255,255,255,1)","type":"none","gradColors":null,"rgradPos":null,"backgroundRotation":null,"contentScale":100,"scaleMode":"crop","useBorder":false,"horizontalAlign":"center","verticalAlign":"middle","contentOffsetX":50,"contentOffsetY":50,"url":null,"hqUrl":null},"duration":12,"id":110,"stopSlide":false,"transition":{"type":"alpha","crosstype":"cross","direction":"r2l","duration":0.1,"wordsDuration":0.5,"wordsAppearOrder":"l2r","delay":0,"tween":"Strong","ease":"easeOut","slidePosX":0,"slidePosY":0,"alphaOffset":0,"slideOffset":0,"blurAmount":10,"zoom":10,"preset":"none"}},"type":"slide","elements":[{"type":"layer","layerType":"shape","properties":{"id":139,"layerName":"Shape 1","dropShadow":{"useShadow":false,"hShadow":2,"vShadow":2,"blur":3,"spread":0,"color":"rgba(0,0,0,0.5)"},"blur":{"useBlur":false,"pixels":2},"opacity":100,"rotation":0,"flip":"none","x":500,"y":300,"width":600,"height":600,"buildIn":{"type":"scale","crosstype":"hide","direction":"center","duration":0.8,"wordsDuration":0.5,"wordsAppearOrder":"l2r","delay":0,"tween":"Strong","ease":"easeOut","slidePosX":50,"slidePosY":0,"alphaOffset":0,"slideOffset":100,"blurAmount":0,"zoom":10,"preset":"grow"},"buildOut":{"type":"zoom","crosstype":"hide","direction":"center","duration":1,"wordsDuration":0.5,"wordsAppearOrder":"l2r","delay":0.1,"tween":"Strong","ease":"easeOut","slidePosX":50,"slidePosY":0,"alphaOffset":0,"slideOffset":100,"blurAmount":0,"zoom":7,"preset":"none"},"actions":[],"visible":true,"group":null,"type":"circle","backgroundColor":{"borderColor":"#3d3d3d","scolor":"#fafafa","type":"solid","gradColors":null,"rgradPos":null,"backgroundRotation":null,"contentScale":100,"scaleMode":"crop","useBorder":false,"horizontalAlign":"center","verticalAlign":"middle","contentOffsetX":50,"contentOffsetY":50,"url":null,"hqUrl":null},"border":{"color":"rgba(0, 0, 0, 1)","weight":0,"radius":300}}},{"type":"layer","layerType":"image","properties":{"id":121,"layerName":"logo 1","dropShadow":{"useShadow":false,"hShadow":2,"vShadow":2,"blur":3,"spread":0,"color":"rgba(0,0,0,0.5)"},"blur":{"useBlur":false,"pixels":2},"opacity":100,"rotation":0,"flip":"none","x":738,"y":521,"width":124,"height":157,"buildIn":{"type":"scale","crosstype":"hide","direction":"center","duration":1,"wordsDuration":1,"wordsAppearOrder":"random","delay":0.2,"tween":"Strong","ease":"easeOut","slidePosX":0,"slidePosY":0,"alphaOffset":0,"slideOffset":50,"blurAmount":0,"zoom":10,"preset":"grow"},"buildOut":{"type":"alpha","crosstype":"hide","direction":"b2t","duration":0.6,"wordsDuration":0.5,"wordsAppearOrder":"l2r","delay":10.2,"tween":"Quad","ease":"easeIn","slidePosX":50,"slidePosY":0,"alphaOffset":0,"slideOffset":50,"blurAmount":0,"zoom":10,"preset":"fade"},"actions":[],"visible":true,"group":null,"url":"f89cd24ab5cd7b61d008a58f74899829","scaleMode":"crop","horizontalAlign":"center","verticalAlign":"middle","adjustColor":{"useAdjustColor":false,"brightness":0,"contrast":0,"saturate":0,"hue":0},"contentScale":100,"contentOffsetX":50,"contentOffsetY":50,"oWidth":140,"oHeight":180,"hqUrl":null,"localUrl":"43f8c21028be612bfa5ac48e3348d005.png"}},{"type":"layer","layerType":"text","properties":{"id":141,"layerName":"Text 1 - Loading EtherMium...","dropShadow":{"useShadow":false,"hShadow":2,"vShadow":2,"blur":3,"spread":0,"color":"rgba(0,0,0,0.5)"},"blur":{"useBlur":false,"pixels":2},"opacity":100,"rotation":0,"flip":"none","x":344,"y":943,"width":912,"height":45,"buildIn":{"type":"alpha","crosstype":"hide","direction":"l2r","duration":0.3,"wordsDuration":0.5,"wordsAppearOrder":"l2r","delay":0.9,"tween":"Quad","ease":"easeOut","slidePosX":50,"slidePosY":0,"alphaOffset":0,"slideOffset":100,"blurAmount":0,"zoom":10,"preset":"fade"},"buildOut":{"type":"alpha","crosstype":"hide","direction":"l2r","duration":0.6,"wordsDuration":0.5,"wordsAppearOrder":"l2r","delay":10.2,"tween":"Quad","ease":"easeIn","slidePosX":50,"slidePosY":0,"alphaOffset":0,"slideOffset":100,"blurAmount":0,"zoom":10,"preset":"fade"},"actions":[],"visible":true,"group":null,"text":"Loading EtherMium...\n","alignment":"center","fontSize":33,"lineHeight":1.4,"letterSpacing":0,"config":[{"offset":0,"length":20,"nodeType":"div","style":{"fontWeight":300,"color":"#000000","fontFamily":"Roboto","fontStyle":"normal","fontType":"google"},"children":[{"offset":0,"length":20,"nodeType":"span","style":null}]}],"textShadow":{"useShadow":false,"hShadow":2,"vShadow":2,"blur":3,"spread":0,"color":"rgba(0,0,0,0.5)"}}},{"type":"layer","layerType":"shape","properties":{"id":140,"layerName":"Shape 2","dropShadow":{"useShadow":false,"hShadow":2,"vShadow":2,"blur":3,"spread":0,"color":"rgba(0,0,0,0.5)"},"blur":{"useBlur":false,"pixels":2},"opacity":100,"rotation":0,"flip":"none","x":500,"y":1018,"width":600,"height":13,"buildIn":{"type":"alpha","crosstype":"hide","direction":"l2r","duration":0.5,"wordsDuration":0.5,"wordsAppearOrder":"l2r","delay":1.1,"tween":"Quad","ease":"easeOut","slidePosX":50,"slidePosY":0,"alphaOffset":0,"slideOffset":100,"blurAmount":0,"zoom":10,"preset":"fade"},"buildOut":{"type":"alpha","crosstype":"hide","direction":"l2r","duration":0.6,"wordsDuration":0.5,"wordsAppearOrder":"l2r","delay":9.8,"tween":"Quad","ease":"easeIn","slidePosX":50,"slidePosY":0,"alphaOffset":0,"slideOffset":100,"blurAmount":0,"zoom":10,"preset":"fade"},"actions":[],"visible":true,"group":null,"type":"rectangle","backgroundColor":{"borderColor":"#3d3d3d","scolor":"#f1f3f8","type":"solid","gradColors":null,"rgradPos":null,"backgroundRotation":null,"contentScale":100,"scaleMode":"crop","useBorder":false,"horizontalAlign":"center","verticalAlign":"middle","contentOffsetX":50,"contentOffsetY":50,"url":null,"hqUrl":null},"border":{"color":"rgba(0, 0, 0, 1)","weight":0,"radius":0}}},{"type":"layer","layerType":"shape","properties":{"id":142,"layerName":"Shape 3","dropShadow":{"useShadow":false,"hShadow":2,"vShadow":2,"blur":3,"spread":0,"color":"rgba(0,0,0,0.5)"},"blur":{"useBlur":false,"pixels":2},"opacity":100,"rotation":0,"flip":"none","x":500,"y":1018,"width":600,"height":13,"buildIn":{"type":"scale","crosstype":"hide","direction":"l2r","duration":10.2,"wordsDuration":0.5,"wordsAppearOrder":"l2r","delay":1.1,"tween":"Strong","ease":"easeOut","slidePosX":50,"slidePosY":0,"alphaOffset":0,"slideOffset":100,"blurAmount":0,"zoom":10,"preset":"none"},"buildOut":{"type":"alpha","crosstype":"hide","direction":"l2r","duration":0.6,"wordsDuration":0.5,"wordsAppearOrder":"l2r","delay":0.1,"tween":"Quad","ease":"easeIn","slidePosX":50,"slidePosY":0,"alphaOffset":0,"slideOffset":100,"blurAmount":0,"zoom":10,"preset":"fade"},"actions":[],"visible":true,"group":null,"type":"rectangle","backgroundColor":{"borderColor":"#3d3d3d","scolor":"#0025f7","type":"solid","gradColors":null,"rgradPos":null,"backgroundRotation":null,"contentScale":100,"scaleMode":"crop","useBorder":false,"horizontalAlign":"center","verticalAlign":"middle","contentOffsetX":50,"contentOffsetY":50,"url":null,"hqUrl":null},"border":{"color":"rgba(0, 0, 0, 1)","weight":0,"radius":0}}}]}]}};
+var bannerConfig = {"photosUrl":"s3.amazonaws.com/newfiles.bannersnack.net/files/","startSlide":0,"noAnimation":false,"preview":false,"showOnlyOneSlide":false,"download":true,"env":"live","embedUrl":"","showWatermark":false};
+
+// loading
+document.documentElement.className = 'js';
+var readyStateCheckInterval = setInterval(function() {
+  if (document.readyState === "complete") {
+    clearInterval(readyStateCheckInterval);
+    document.body.classList.remove('loading');
+  }
+}, 10);
+// /loading
+
+// Intercom
+window.intercomSettings = {
+  app_id: 'y1l3is5h',
+  hide_default_launcher: true,
+};
+(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;
+s.src='https://widget.intercom.io/widget/y1l3is5h';
+var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
+// /Intercom
+
+// Yandex.Metrika counter
+(function (d, w, c) {
+    (w[c] = w[c] || []).push(function() {
+        try {
+            w.yaCounter50578021 = new Ya.Metrika2({
+                id:50578021,
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true,
+                webvisor:true
+            });
+        } catch(e) { }
+    });
+
+    var n = d.getElementsByTagName("script")[0],
+        s = d.createElement("script"),
+        f = function () { n.parentNode.insertBefore(s, n); };
+    s.type = "text/javascript";
+    s.async = true;
+    s.src = "https://mc.yandex.ru/metrika/tag.js";
+
+    if (w.opera == "[object Opera]") {
+        d.addEventListener("DOMContentLoaded", f, false);
+    } else { f(); }
+})(document, window, "yandex_metrika_callbacks2");
+// /Yandex.Metrika counter
